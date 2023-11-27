fa8b7b7a3cd47e92f40d3c7cd167f793d263cb0d
George Omondi <omondigeorge.r@gmail.com>
2023-11-27 22:53:24 +0300

    Changed route names for portfolio page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
 import Home from "./Home.jsx";
 import Contact from "./Contact.jsx";
-import CV from './CV.jsx';
+import CV from "./CV.jsx";
 import Portfolio from "./Portfolio.jsx";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-        <header>
-          <nav>
-            <NavLink to="/home">Home</NavLink>
-            <NavLink to="/cv">CV</NavLink>
-            <NavLink to="/portfolio">Portfolio</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-          </nav>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/cv" element={<CV />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/cv">CV</NavLink>
+          <NavLink to="/portfolio-page">Portfolio</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cv" element={<CV />} />
+          <Route path="/portfolio-page" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
